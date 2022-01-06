7484d2ff71db540354ad3a0eadd5cd20001ada5a
Kavishka Sulakshana <kss20001218@gmail.com>
2022-01-06 07:09:33 +0530

    Topological sort

--- topologicalSort.py
+++ topologicalSort.py
@@ -6,13 +6,13 @@ Graph = {'A': ['C', 'D'],
          'F': [],
          'G': []}
 
-sortedList = []
-num = {}
-TSNum = {}
+sortedList = []  # this is the sorted list
+num = {}    # This is the time discovered by each vertex
+TSNum = {}  # This is the time based on sort
 i = j = 1
 
 
-def T_sort(v):
+def T_sort(v):  # recursive function
     global i, j
     num[v] = i
     i += 1
@@ -20,6 +20,7 @@ def T_sort(v):
         if num[u] == 0:
             T_sort(u)
         elif TSNum[u] == 0:
+            # if num(u) == 0 and TSNum[u] == 0 then graph has a cycle | not a DAG (Directed Acyclic Graph)
             print('error | cycle detected')
     TSNum[v] = j
     j += 1
@@ -28,6 +29,7 @@ def T_sort(v):
 
 def topological_sort(graph):
     for v in graph:
+        # The discovered time and sorted time for all vertices are set to 0
         num[v] = 0
         TSNum[v] = 0
     for v in graph:
